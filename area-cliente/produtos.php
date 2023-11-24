a670076e115567b21ec484383fd2c3955dce05f3
Kazuto15 <kauadasilvabarbosa06@gmail.com>
2023-11-24 18:58:09 -0300

    Update produtos.php

--- area-cliente/produtos.php
+++ area-cliente/produtos.php
@@ -63,6 +63,32 @@
                 <p>Preco: R$19,99</p>
             </div>
         </div>
-        
+
+            <div class="carousel-container" style="display: none;">
+                <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="card-item">Conteúdo do Carrossel 1</div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="card-item">Conteúdo do Carrossel 2</div>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Anterior</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Próximo</span>
+                    </button>
+                </div>
+            </div>
+
+
+
+
+        <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI/tt1U3lSJqFK2AA4VVaKuOZlNl+CaXz2aXFr8=" crossorigin="anonymous"></script>
+
 </body>
 </html>
